6e99fc7dfba164e5b05776e0d2917aeb379107ca
Max Shaptala <maxshaptala@gmail.com>
2022-01-16 01:49:06 +0200

    Added demo code for task03

---
+++ src/main/java/com/softserveinc/task03/Main.java
@@ -0,0 +1,39 @@
+package com.softserveinc.task03;
+
+// TODO: import the class City created in previous task
+
+import java.util.Scanner;
+
+/**
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        // TODO: Uncomment code bellow after the class Country have been created
+//        UserManager userManager = new UserManager();
+//        userManager.register("admin", "qwerty");
+//        userManager.register("user", "12345");
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Login: ");
+//        String login = scanner.nextLine();
+//        System.out.print("Password: ");
+//        String password = scanner.nextLine();
+//
+//        if (userManager.exists(login, password)) {
+//            System.out.println("Welcome " + login);
+//            System.out.println("Change your password");
+//            System.out.print("Old password: ");
+//            String oldPassword = scanner.nextLine();
+//            System.out.print("New password: ");
+//            String newPassword = scanner.nextLine();
+//            if (userManager.setPassword(login, oldPassword, newPassword)) {
+//                System.out.println("Your password has been changed");
+//            } else {
+//                System.err.println("Password hasn't been changed because old password incorrect");
+//            }
+//        } else {
+//            System.err.println("Access denied");
+//        }
+    }
+}
